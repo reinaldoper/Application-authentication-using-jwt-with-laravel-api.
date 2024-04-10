ea75fb48128840b50748c0bff66f548f2ad060c3
reinaldoper <reinaldoper83@gmail.com>
2024-04-10 12:31:47 -0400

    feat: adiciona UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,10 @@ class UserController extends Controller
 {
     public function profile(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string',
+            'userId' => 'required|integer|exists:users,id', 
+        ]);
         User::where('id', auth()->user()->id)->update(
             [
                 'name' => $request->input('name'),
